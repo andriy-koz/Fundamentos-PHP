7bb85bd2b70f9ac28168506b40967546b5f30b81
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-07-15 15:48:10 -0300

    tipos de dato

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,23 @@
 <?php include 'includes/header.php';
 
+// boolean
+$logueado = true;
+var_dump($logueado);
 
+// enteros
+$numero = 20;
+var_dump($numero);
 
+// float
+$flotante = 20.5;
+var_dump($flotante);
+
+// string 
+$nombre = "Andriy";
+var_dump($nombre);
+
+// array
+$array = [];
+var_dump($array);
 
 include 'includes/footer.php';
