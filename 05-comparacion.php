28ac5fb00dabfed83e98a74e7c3d9bafe7ff1685
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-07-15 16:14:43 -0300

    comparacion

--- 05-comparacion.php
+++ 05-comparacion.php
@@ -1,7 +1,34 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 30;
+$numero3 = "30";
 
+var_dump($numero1 > $numero2);
+echo "<br/>";
 
+var_dump($numero1 < $numero2);
+echo "<br/>";
 
+var_dump($numero1 >= $numero2);
+echo "<br/>";
+
+var_dump($numero1 <= $numero2);
+echo "<br/>";
+
+var_dump($numero2 == $numero3);
+echo "<br/>";
+
+var_dump($numero2 === $numero3);
+echo "<br/>";
+
+var_dump($numero1 <=> $numero2);
+echo "<br/>";
+
+var_dump($numero2 <=> $numero1);
+echo "<br/>";
+
+var_dump($numero2 <=> $numero2);
+echo "<br/>";
 
 include 'includes/footer.php';
