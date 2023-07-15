d9333fb62669336b7d2fd91ae236ff1db84a7037
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-07-15 16:21:48 -0300

    incrementos

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+$numero1 = 30;
 
+$numero1++;
 
+echo $numero1;
+echo "<br/>";
+
+echo $numero1++;
+echo "<br/>";
+
+echo $numero1;
+echo "<br/>";
+
+echo ++$numero1;
+echo "<br/>";
+
+echo $numero1;
+echo "<br/>";
+
+$numero1--;
+
+echo $numero1;
+echo "<br/>";
+
+$numero1 += 5;
+echo $numero1;
 
 include 'includes/footer.php';
