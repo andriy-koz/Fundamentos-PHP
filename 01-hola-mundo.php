da4afee31de99f4b867ec85b583c35f607c1f9a6
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-07-15 15:21:45 -0300

    01 done

--- 01-hola-mundo.php
+++ 01-hola-mundo.php
@@ -1,9 +1,15 @@
 <?php
 
+include 'includes/header.php';
+
 echo "Hola mundo <br>";
 
 print "Hola mundo <br>";
 
 print_r("Hola mundo <br>");
 
-?>
+var_dump("Hola mundo");
+
+echo "<br>";
+
+include 'includes/footer.php';
