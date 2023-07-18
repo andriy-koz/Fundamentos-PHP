7083c444c2124034db005a40dbf25d1ba7853f2f
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-07-17 22:42:16 -0300

    funciones arreglos

--- 11-funciones-arrays.php
+++ 11-funciones-arrays.php
@@ -1,7 +1,50 @@
 <?php include 'includes/header.php';
 
+// buscar elementos en un arrelgo
+$numeros = [1, 2, 3];
 
+echo "<pre>";
+var_dump(in_array(2, $numeros));
+echo "</pre>";
 
+// ordenar elementos de un arreglo de menor a mayor
+sort($numeros);
+echo "<pre>";
+var_dump($numeros);
+echo "</pre>";
 
+// ordenar elementos de una arreglo de mayor a menor
+rsort($numeros);
+echo "<pre>";
+var_dump($numeros);
+echo "</pre>";
+
+// ordenar arreglo asociativo por valores
+$cliente = [
+  'nombre' => 'Andriy',
+  'tipo' => 'Premium',
+  'saldo' => 200,
+];
+
+asort($cliente);
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+arsort($cliente);
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// ordenar arreglos asociativo por llaves
+ksort($cliente);
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+krsort($cliente);
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
