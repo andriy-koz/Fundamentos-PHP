300e136ff68f491555091083af9a3f308d1742ea
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-07-17 22:28:09 -0300

    empty isset

--- 10-isset-empty.php
+++ 10-isset-empty.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+// crear varios arreglos
+$cliente1 = [];
+$cliente2 = array();
+$cliente3 = ['nombre' => 'Andriy', 'cel' => '12345'];
 
+// usar 'empty'
 
+echo "<pre>";
+var_dump(empty($cliente1));
+echo "</pre>";
+
+echo "<pre>";
+var_dump(empty($cliente3));
+echo "</pre>";
+
+// usar 'isset'
+
+echo "<pre>";
+var_dump(isset($cliente3['codigo']));
+echo "</pre>";
+
+echo "<pre>";
+var_dump(isset($cliente1));
+echo "</pre>";
 
 include 'includes/footer.php';
